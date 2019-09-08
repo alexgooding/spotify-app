93cc6a0fbd97bda421b83b273bc6d87958c8fff5
Alex Gooding <alexxgooding@gmail.com>
2019-09-08 14:44:30 +0100

    change scope of access

--- src/helper_methods.py
+++ src/helper_methods.py
@@ -7,7 +7,11 @@ class SpotifyHelper:
     def __init__(self, username='qsnyvybl8iarj07dojit7xon1'):
 
         self.username = username
-        self.scope = 'user-library-read'
+        self.scope = 'playlist-read-collaborative playlist-modify-private playlist-modify-public ' \
+                     'playlist-read-private user-modify-playback-state user-read-currently-playing ' \
+                     'user-read-playback-state user-read-private user-read-email user-library-modify ' \
+                     'user-library-read user-follow-modify user-follow-read user-read-recently-played user-top-read ' \
+                     'streaming app-remote-control'
         self.token = util.prompt_for_user_token(username, self.scope, client_id='b6227692e279468b8dc73975240df0b3',
                                                 client_secret='459cb2f1c81146c0b6eaffbded50f2a2',
                                                 redirect_uri='https://open.spotify.com/collection/playlists')
